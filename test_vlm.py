b744e8bd7977b6a53037d7764618fecced36c876
ruhu_msi <fred9076@gmail.com>
2024-12-12 13:30:53 +0100

    update

--- test_vlm.py
+++ test_vlm.py
@@ -1,38 +1,62 @@
-from modelscope import (
-    snapshot_download, AutoModelForCausalLM, AutoTokenizer, GenerationConfig
+from transformers import Qwen2VLForConditionalGeneration, AutoTokenizer, AutoProcessor
+from qwen_vl_utils import process_vision_info
+from modelscope import snapshot_download
+model_dir = snapshot_download("qwen/Qwen2-VL-7B-Instruct")
+
+# default: Load the model on the available device(s)
+model = Qwen2VLForConditionalGeneration.from_pretrained(
+    model_dir, torch_dtype="auto", device_map="auto"
 )
-import torch
-model_id = 'qwen/Qwen2-VL-7B-Instruct'
 
-model_dir = snapshot_download(model_id)
-torch.manual_seed(1234)
+# We recommend enabling flash_attention_2 for better acceleration and memory saving, especially in multi-image and video scenarios.
+# model = Qwen2VLForConditionalGeneration.from_pretrained(
+#     model_dir,
+#     torch_dtype=torch.bfloat16,
+#     attn_implementation="flash_attention_2",
+#     device_map="auto",
+# )
 
-# 请注意：分词器默认行为已更改为默认关闭特殊token攻击防护。
-tokenizer = AutoTokenizer.from_pretrained(model_dir, trust_remote_code=True)
-# 打开bf16精度，A100、H100、RTX3060、RTX3070等显卡建议启用以节省显存
-# model = AutoModelForCausalLM.from_pretrained(model_dir, device_map="auto", trust_remote_code=True, bf16=True).eval()
-# 打开fp16精度，V100、P100、T4等显卡建议启用以节省显存
-model = AutoModelForCausalLM.from_pretrained(model_dir, device_map="auto", trust_remote_code=True, fp16=True).eval()
-# 使用CPU进行推理，需要约32GB内存
-# model = AutoModelForCausalLM.from_pretrained(model_dir, device_map="cpu", trust_remote_code=True).eval()
-# 默认使用自动模式，根据设备自动选择精度
-# model = AutoModelForCausalLM.from_pretrained(model_dir, device_map="auto", trust_remote_code=True).eval()
+# default processer
+processor = AutoProcessor.from_pretrained(model_dir)
 
-# 可指定不同的生成长度、top_p等相关超参
-model.generation_config = GenerationConfig.from_pretrained(model_dir, trust_remote_code=True)
+# The default range for the number of visual tokens per image in the model is 4-16384. You can set min_pixels and max_pixels according to your needs, such as a token count range of 256-1280, to balance speed and memory usage.
+# min_pixels = 256*28*28
+# max_pixels = 1280*28*28
+# processor = AutoProcessor.from_pretrained(model_dir, min_pixels=min_pixels, max_pixels=max_pixels)
 
-# 第一轮对话 1st dialogue turn
-query = tokenizer.from_list_format([
-    {'image': 'https://qianwen-res.oss-cn-beijing.aliyuncs.com/Qwen-VL/assets/demo.jpeg'},
-    {'text': '这是什么'},
-])
-response, history = model.chat(tokenizer, query=query, history=None)
-print(response)
-# 图中是一名年轻女子在沙滩上和她的狗玩耍，狗的品种是拉布拉多。她们坐在沙滩上，狗的前腿抬起来，与人互动。
+messages = [
+    {
+        "role": "user",
+        "content": [
+            {
+                "type": "image",
+                "image": "https://qianwen-res.oss-cn-beijing.aliyuncs.com/Qwen-VL/assets/demo.jpeg",
+            },
+            {"type": "text", "text": "Describe this image."},
+        ],
+    }
+]
 
-# 第二轮对话 2st dialogue turn
-response, history = model.chat(tokenizer, '输出击掌的检测框', history=history)
-print(response)
-# <ref>"击掌"</ref><box>(211,412),(577,891)</box>
-image = tokenizer.draw_bbox_on_latest_picture(response, history)
-image.save('output_chat.jpg')
+# Preparation for inference
+text = processor.apply_chat_template(
+    messages, tokenize=False, add_generation_prompt=True
+)
+image_inputs, video_inputs = process_vision_info(messages)
+inputs = processor(
+    text=[text],
+    images=image_inputs,
+    videos=video_inputs,
+    padding=True,
+    return_tensors="pt",
+)
+inputs = inputs.to("cuda")
+
+# Inference: Generation of the output
+generated_ids = model.generate(**inputs, max_new_tokens=128)
+generated_ids_trimmed = [
+    out_ids[len(in_ids) :] for in_ids, out_ids in zip(inputs.input_ids, generated_ids)
+]
+output_text = processor.batch_decode(
+    generated_ids_trimmed, skip_special_tokens=True, clean_up_tokenization_spaces=False
+)
+print(output_text)
